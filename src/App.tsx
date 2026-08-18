67e409953e2484edffb21eb5e068ebd7663a50ed
Dyad <andfariiia@gmail.com>
2026-08-18 08:45:38 -0300

    Remoção da rota duplicada de área de membros - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,6 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/membros" element={<Members />} />
-          <Route path="/area-membros" element={<Members />} />
           <Route path="/obrigado" element={<Obrigado />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
